d87057996d0d6b9cf65c9fb642874a388bf76a40
Davwidos <d.jurkowski015@gmail.com>
2024-06-10 14:46:14 +0200

    solution

--- src/app.js
+++ src/app.js
@@ -1,1 +1,36 @@
-// write code here
+/* eslint-disable no-console */
+const fs = require('fs/promises');
+const path = require('path');
+
+(async () => {
+  if (process.argv.length < 4) {
+    console.error();
+
+    return;
+  }
+
+  try {
+    const [filePath, destination] = process.argv.slice(2);
+    const fileName = path.basename(filePath);
+    let newFilePath = destination;
+
+    await fs.access(filePath, fs.constants.F_OK);
+
+    const isDir = await fs
+      .lstat(destination)
+      .then((stat) => stat.isDirectory())
+      .catch(() => false);
+
+    if (destination.endsWith(path.sep) && !isDir) {
+      throw new Error();
+    }
+
+    if (isDir) {
+      newFilePath = path.join(newFilePath, fileName);
+    }
+
+    await fs.rename(filePath, newFilePath);
+  } catch (e) {
+    console.error(e);
+  }
+})();
